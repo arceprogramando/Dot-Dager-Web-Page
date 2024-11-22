075905279898bec0c42d9b0fa94ad51e1d2fd00f
Felipe Arce <arceprogramando@gmail.com>
2024-11-22 14:56:20 -0300

    feat: create pages

---
+++ src/data/components/links.ts
@@ -0,0 +1,11 @@
+export interface Link {
+  href: string;
+  title: string;
+}
+
+export const linksData: Link[] = [
+  { href: '/game', title: 'Juego' },
+  { href: '/images/', title: 'Imagenes' },
+  { href: '/videos', title: 'Videos' },
+  { href: '/networks', title: 'Redes' },
+];
